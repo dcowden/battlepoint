3ce83cfaa8063fb7cfdc09e15889e684ae88224a
Dave Cowden <dave.cowden@gmail.com>
2021-09-07 21:29:56 -0400

    added target scanner

--- target/platformio/lib/battlepoint/target.h
+++ target/platformio/lib/battlepoint/target.h
@@ -10,8 +10,11 @@
 //ESP32 ADC : 6k samples/sec. 128 samples -> 21.3333 ms
 //noisy part of the signal was 700hz, about 7 peaks in 20ms
 //smooth part was about 116hz
-#define TARGET_FFT_SAMPLES 512
+#define TARGET_FFT_SAMPLES 128
 #define BP_DEBUG 1
+
+
+
 
 typedef struct {
     long last_hit_millis;
